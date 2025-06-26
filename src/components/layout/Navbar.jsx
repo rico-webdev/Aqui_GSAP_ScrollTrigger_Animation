d5fe4257c807e3874de4f49d572d5c461d555a3a
ricotech.dev <151479600+rico-webdev@users.noreply.github.com>
2025-06-27 00:55:25 +0200

    feat(navbar): add navbar gsap init animation

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -19,6 +19,13 @@ const Navbar = () => {
   const isAnimating = useRef(false);
 
   useGSAP(() => {
+    gsap.from(navRef.current, {
+      y: -100,
+      opacity: 0,
+      delay: 1.91,
+      duration: 0.6,
+      ease: "power1.out",
+    });
     ScrollTrigger.create({
       start: 0,
       end: "max",
@@ -32,7 +39,7 @@ const Navbar = () => {
             y: 0,
             backgroundColor: "#00000050",
             backdropFilter: "blur(8px)",
-            ease: "power3.in",
+            ease: "power1.out",
             duration: 0.7,
             onComplete: () => {
               isAnimating.current = false;
@@ -45,7 +52,7 @@ const Navbar = () => {
 
           gsap.to(navRef.current, {
             y: "-100%",
-            ease: "power3.out",
+            ease: "power1.out",
             duration: 0.7,
             onComplete: () => {
               isAnimating.current = false;
@@ -57,17 +64,19 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav ref={navRef} className="fixed top-0 left-0 w-full z-50">
-      <div>
-        <a href="/" className="flex-center gap-2">
+    <nav
+      ref={navRef}
+      className="fixed top-0 left-0 z-50 w-full font-extralight px-4 md:px-8 pb-4 md:pb-8">
+      <div className="container col-center gap-5 md:flex-row md:justify-between py-5 mx-auto">
+        <Link href="/" className="row-center gap-2">
           <Image src="/images/logo.png" alt="Logo" width={20} height={20} />
-          <p>Aqui - Pub and Bar</p>
-        </a>
-        <ul>
+          <p className="tracking-wide">Aqui - Pub and Bar</p>
+        </Link>
+        <ul className="row-center gap-4 lg:gap-12">
           {navLinks.map((link) => (
             <li key={link.id}>
               <Link href={link.href}>
-                <p>{link.title}</p>
+                <p className="tracking-widest">{link.title}</p>
               </Link>
             </li>
           ))}
