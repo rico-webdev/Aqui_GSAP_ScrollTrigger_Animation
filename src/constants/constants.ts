92fd29ea8e91a0e6bbf29862e2a3992d52bc34bc
ricotech.dev <151479600+rico-webdev@users.noreply.github.com>
2025-06-30 23:10:03 +0200

    feat(types): create shared interface definitions for app structure

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,24 +1,111 @@
-const navLinks = [
+import { MenuItem, MenuSection, NavLink } from "@/types";
+
+export const navLinks: NavLink[] = [
   {
-    id: "cocktails",
-    title: "Cocktails",
-    href: "/cocktails",
+    id: "menu",
+    title: "Menu",
+    href: "#menu",
   },
   {
     id: "about",
     title: "About Us",
-    href: "/about",
+    href: "#about",
   },
   {
     id: "work",
     title: "The Art",
-    href: "/work",
+    href: "#work",
   },
   {
     id: "contact",
     title: "Contact",
-    href: "/contact",
+    href: "#contact",
   },
 ];
 
-export { navLinks };
+export const menu: {
+  cocktails: MenuItem[];
+  beers: MenuItem[];
+} = {
+  cocktails: [
+    {
+      id: "cocktail-1",
+      name: "Mojito",
+      origin: "Cuba",
+      alcohol: true,
+      serving: "Glass",
+      method: "Frozen",
+      price: 10,
+    },
+    {
+      id: "cocktail-2",
+      name: "Margarita",
+      origin: "Mexico",
+      alcohol: true,
+      serving: "Glass",
+      method: "Blended",
+      price: 12,
+    },
+    {
+      id: "cocktail-3",
+      name: "Old Fashioned",
+      origin: "USA",
+      alcohol: true,
+      serving: "Glass",
+      method: "Blended",
+      price: 14,
+    },
+    {
+      id: "cocktail-4",
+      name: "Negroni",
+      origin: "Italy",
+      alcohol: true,
+      serving: "Glass",
+      method: "On the Rocks",
+      price: 13,
+    },
+  ],
+  beers: [
+    {
+      id: "beer-1",
+      name: "Pilsner",
+      origin: "Germany",
+      alcohol: true,
+      serving: "Pint",
+      method: "Draft",
+      price: 7,
+    },
+    {
+      id: "beer-2",
+      name: "Wheat Beer",
+      origin: "Belgium",
+      alcohol: true,
+      serving: "Glass",
+      method: "Draft",
+      price: 8,
+    },
+    {
+      id: "beer-3",
+      name: "IPA",
+      origin: "USA",
+      alcohol: true,
+      serving: "Pint",
+      method: "Draft",
+      price: 8,
+    },
+    {
+      id: "beer-4",
+      name: "Stout",
+      origin: "Ireland",
+      alcohol: true,
+      serving: "Pint",
+      method: "Draft",
+      price: 9,
+    },
+  ],
+};
+
+export const menuSections: MenuSection[] = [
+  { title: `cocktails`, items: menu.cocktails, id: "cocktails" },
+  { title: `beers`, items: menu.beers, id: "beers" },
+];
---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+export type ServingMethod = "Draft" | "Bottle" | "On the Rocks" | "Blended" | "Frozen";
+export type ServingSize = "Pint" | "Glass";
+
+export interface MenuSection {
+  title: string;
+  items: MenuItem[];
+  id: string;
+}
+
+export interface MenuItem {
+  id: string;
+  name: string;
+  origin: string;
+  alcohol: boolean;
+  serving: ServingSize;
+  method: ServingMethod;
+  price: number;
+}
+
+export interface NavLink {
+  id: string;
+  title: string;
+  href: `#${string}`;
+}
