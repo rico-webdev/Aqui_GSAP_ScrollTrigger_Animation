c63f1d7c282d620557b068b2bfc45c7e223f86c9
ricotech.dev <151479600+rico-webdev@users.noreply.github.com>
2025-06-30 23:04:28 +0200

    refactor(hero): simplify leaf animation using ScrollTrigger `data-speed` instead of gsap.to()

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { useGSAP } from "@/plugins/index";
 
 import dynamic from "next/dynamic";
-import { initAnimation, leafAnimation } from "@/utils/animations/textAnimations";
+import { initAnimation } from "@/utils/animations/textAnimations";
 const VideoScrollAnim = dynamic(() => import("./VideoScrollAnim"), {
   ssr: false,
 });
@@ -18,53 +18,20 @@ const Hero = () => {
     initAnimation();
   }, []);
 
-  useGSAP(
-    () => {
-      leafAnimation();
-    },
-    { scope: heroSection }
-  );
-
   return (
     <div className="relative" ref={heroSection}>
       <section
         id="hero"
         className="relative z-10 min-h-dvh flex flex-col md:justify-between px-4 lg:px-8 pb-4 lg:pb-8 pt-25">
-        <h1
-          id="title"
-          className="text-[clamp(6rem,7vw,10rem)] text-center font-bold mb-5 font-cinzel">
+        <h1 id="title" className="text-[clamp(6rem,15vw,12rem)] text-center font-bold font-cinzel">
           Aquí
         </h1>
-
-        {/* leaf images here */}
-        <Image
-          id="left-leaf"
-          src="/images/hero-left-leaf.png"
-          alt="green leaf"
-          width={228}
-          height={478}
-          priority={false}
-          loading="lazy"
-          className="absolute left-0 -bottom-20 md:top-40 w-[30%] md:w-1/5"
-        />
-        <div>
-          <Image
-            id="right-leaf"
-            src="/images/hero-right-leaf.png"
-            alt="green leaf"
-            width={266}
-            height={661}
-            priority={false}
-            loading="lazy"
-            className="absolute right-0 top-[20] w-1/4 md:w-1/6"
-          />
-        </div>
 
         {/* content here */}
         <div className="container w-full mx-auto col-center md:flex-row md:justify-between">
           <div id="slogan" className="hidden md:block text-center">
             <p>Bold. Local. Unforgettable.</p>
-            <p className="text-[clamp(2rem,5vw,4rem)] text-yellow">
+            <p className="text-[clamp(2rem,5vw,4rem)] text-highlight">
               The Soul of
               <span className="block -mt-5 font-dancing-script">the City</span>
             </p>
@@ -76,7 +43,7 @@ const Hero = () => {
             </p>
             <Link
               id="view-cocktails-link"
-              href="/cocktails"
+              href="#cocktails"
               className="font-semibold text-right hover:text-hover">
               View cocktails
             </Link>
@@ -87,6 +54,32 @@ const Hero = () => {
       {/* animated video */}
       <div id="video" className="absolute inset-0">
         <VideoScrollAnim />
+      </div>
+
+      {/* leaf images here */}
+      <Image
+        id="left-leaf"
+        src="/images/leaf.png"
+        alt="green leaf"
+        width={228}
+        height={478}
+        priority={false}
+        loading="lazy"
+        className="absolute -left-10 top-1/3 w-[30%] md:w-[18%] transform rotate-45"
+        data-speed="1.35"
+      />
+      <div>
+        <Image
+          id="right-leaf"
+          src="/images/leaf.png"
+          alt="green leaf"
+          width={266}
+          height={661}
+          priority={false}
+          loading="lazy"
+          className="absolute -right-10 top-[50] w-[20%] transform -rotate-135"
+          data-speed="1.2"
+        />
       </div>
     </div>
   );
